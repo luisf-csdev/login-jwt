daf4a10d1a1831cf5ab7c1797fc42c4f2390f9f9
Luís Felipe <luisf.csdev@gmail.com>
2022-11-16 23:32:37 -0300

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
-import Head from 'next/head'
-import '../styles/global.css'
+import Head from 'next/head';
+import '../styles/global.css';
 
 function App({ Component, pageProps }) {
     return (
         <>
             <Head>
+                <title>Login JWT</title>
+                <link
+                    rel="shortcut icon"
+                    href="/images/favicon.ico"
+                />
                 <link
                     rel="stylesheet"
                     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
@@ -12,7 +17,7 @@ function App({ Component, pageProps }) {
             </Head>
             <Component {...pageProps} />
         </>
-    )
+    );
 }
 
 export default App;
